ea008af537d32283e65ebfd01a98d5c38b94dadd
Sergey Voychenko <s.vouchenko@gmail.com>
2022-04-15 15:49:47 +0300

    noted the comment and corrected

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-for (let num = 0, i = 1; ; num++) {
-    if (i) alert(`Привет! счетчик итераций:(${num})`);
+for (let num = 0 /*i = 1*/; ; num++) {
+    // данная провека не нужна если aler опустить вниз блока
+    // if (i) alert(`Привет! счетчик итераций:(${num})`);
     let words = prompt();
     if (words === 'break') {
         break;
     } else if (words === 'continue') {
-        i = 0;
+        // i = 0;
         continue;
     }
-    i++;
+    // i++;
+    alert(`Привет! счетчик итераций:(${num})`);
 }
